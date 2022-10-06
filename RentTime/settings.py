b3876388a42b70016ac38bcf65e4ca66ae90074f
SadrulToaha <sadruli@numberskills.se>
2022-10-06 23:53:01 +0600

    update

--- RentTime/settings.py
+++ RentTime/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,6 +154,15 @@ LOGOUT_REDIRECT_URL = "home"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Messages built-in framework
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 
 
--- app/views/rents.py
+++ app/views/rents.py
@@ -1,19 +1,20 @@
 from msilib.schema import CreateFolder
 from django.http import HttpRequest, HttpResponseRedirect
 from numpy import append
-from app.views import owners
+from app.decorators import *
 from ..models import *
 from ..forms import *
 from ..filters import *
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def index(request, option):
     if(str(option).lower() == "shared"):
         rents = Rent.objects.filter(is_shared=True, is_deleted=False, is_booked=False).order_by('-created_date')
     else:
-        rents = Rent.objects.filter(is_shared=False, is_deleted=False, is_booked=False).order_by('-created_date')
+        rents = Rent.objects.filter(is_deleted=False, is_booked=False).order_by('-created_date')
 
     myFilter = RentFilter(request.GET, queryset=rents)
     rents = myFilter.qs
@@ -23,20 +24,22 @@ def index(request, option):
     }
     return render(request, 'index.html', context)
 
+@login_required
 def details(request, pk):
     rent = get_object_or_404(Rent, pk=pk)
     return render(request, 'rent-details.html', {'rent': rent})
 
+@owner_required
 def delete(request, pk):
     rent = get_object_or_404(Rent, pk=pk).delete()
     return HttpResponseRedirect('/rents/all/')
 
+@owner_required
 def rent_new(request):
     if request.method == "POST":
         form = RentForm(request.POST, request.FILES)
         if form.is_valid():
             rent = form.save(commit=False)
-
             rent.owner = request.user
             rent.save()
             return redirect('rent_details', pk=rent.pk)
@@ -44,6 +47,7 @@ def rent_new(request):
         form = RentForm()
     return render(request, 'rent-edit.html', {'form': form, 'req':request})
 
+@owner_required
 def rent_edit(request, pk):
     rent = get_object_or_404(Rent, pk=pk)
     if request.method == "POST":
@@ -58,6 +62,7 @@ def rent_edit(request, pk):
         form = RentForm(instance=rent)
     return render(request, 'rent-edit.html', {'form': form, 'req':request})
 
+@renter_required
 def add_rent_request(request, pk):
     requested_flat = get_object_or_404(Rent, pk=pk)
     requested_by = request.user
@@ -68,6 +73,7 @@ def add_rent_request(request, pk):
     rent_req.save()
     return HttpResponseRedirect('/requests/')
 
+@login_required
 def delete_rent_request(request, pk):
     requested_item = get_object_or_404(Request, pk=pk)
     requested_item.is_deleted = True
@@ -75,6 +81,7 @@ def delete_rent_request(request, pk):
     requested_item.save()
     return HttpResponseRedirect('/requests/')
 
+@owner_required
 def accept_rent_request(request, pk):
     requested_item = get_object_or_404(Request, pk=pk)
     requested_item.is_accepted = True
@@ -90,6 +97,7 @@ def accept_rent_request(request, pk):
 
     return HttpResponseRedirect('/requests/')
 
+@renter_required
 def add_roommate_status(request, pk):
     rent = get_object_or_404(Rent, pk=pk)
     rent.is_shared = True
@@ -98,6 +106,7 @@ def add_roommate_status(request, pk):
     rent.save()
     return HttpResponseRedirect('/')
 
+@login_required
 def requested_rents(request):
     if request.user.is_owner:
         requests = Request.objects.filter(flat__in = list(Rent.objects.values_list('id', flat=True).filter(owner = request.user)), is_deleted = False )
@@ -107,12 +116,15 @@ def requested_rents(request):
        
     else:
         requests = Request.objects.filter(flat__in = list(Rent.objects.values_list('id', flat=True)), renter = request.user)
+        
     return render(request, 'requests.html', {'requests': requests})
 
+@owner_required
 def added_rents(request):
     rents = Rent.objects.filter( owner = request.user, is_deleted=False).order_by('-created_date')
     return render(request, 'listings.html', {'rents': rents})
 
+@renter_required
 def approved_bookings(request):
     requests = Request.objects.filter(flat__in = list(Rent.objects.values_list('id', flat=True)), renter = request.user, is_accepted = True)
     return render(request, 'requests.html', {'requests': requests})
